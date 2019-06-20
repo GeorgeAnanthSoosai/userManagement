d09f1d6fdd042b65911d6c8a14ae37a34efdfcfd
George Ananth Soosai <georgeananth.prolog@gmail.com>
2019-06-20 18:55:23 -0400

    setup Angular/Materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserMaterialModule } from './core-module/user-material/user-material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    UserMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
