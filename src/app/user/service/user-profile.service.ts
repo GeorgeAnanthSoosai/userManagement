604653d84c0f5af47f8e1c1fc2239008f7f4d0c8
George Soosai <george_a_soosai@homedepot.com>
2019-06-21 14:48:23 -0400

    basic data setups

--- src/app/user/service/user-profile.service.ts
+++ src/app/user/service/user-profile.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { UserProfile } from '../model/user-profile';
+import { Subject, Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserProfileService {
-  private userProfileList: UserProfile[];
+  private userProfileList: UserProfile[] = [
+    new UserProfile('George', ['Pavan', 'Mary', 'Sam', 'Rinkesh'], 31, 140),
+    new UserProfile('Pavan', ['Mary', 'Kiran'], 45, 120)
+  ];
 
-  constructor() { }
+  userProfileSubject = new BehaviorSubject<UserProfile[]>(this.userProfileList);
+
+  constructor() {}
 
   addNewUser(user: UserProfile) {
     this.userProfileList.push(user);
+    this.userProfileSubject.next(this.userProfileList);
   }
-  getUsers(): UserProfile[]  {
-    return this.userProfileList;
+  getUsers(): Observable<UserProfile[]>  {
+    return this.userProfileSubject.asObservable();
   }
 }
--- src/app/user/user-home/user-home.component.ts
+++ src/app/user/user-home/user-home.component.ts
@@ -3,8 +3,9 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import {fas, faPlusCircle} from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
-
-
+import { UserProfileService } from '../service/user-profile.service';
+import { UserProfile } from '../model/user-profile';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-home',
@@ -12,25 +13,22 @@ import { library } from '@fortawesome/fontawesome-svg-core';
   styleUrls: ['./user-home.component.scss']
 })
 export class UserHomeComponent implements OnInit {
-  userData = [{
-    name : 'George',
-    friends : ['Pavan', 'Mary', 'Sam', 'Rinkesh'],
-    age : 31,
-    weight: 140
-  }];
+  userProfileList: Observable<UserProfile[]>;
 
-  constructor(private modalService: NgbModal) {
+  constructor(private modalService: NgbModal, private userProfileServoice: UserProfileService) {
     library.add(fas, faCoffee, faPlusCircle);
    }
 
   ngOnInit() {
+    this.userProfileList = this.userProfileServoice.getUsers();
   }
 
   createUser(content) {
     this.modalService.open(content, { size: 'lg', backdropClass: 'light-blue-backdrop' })
     .result.then(
       (data) => {
-        console.log(data);
+        console.log(data)
+        this.userProfileServoice.addNewUser(new UserProfile('George', ['Pavan', 'Mary', 'Sam', 'Rinkesh'], 31, 140));
       },
       (dismiss) => {
         console.log('Test', dismiss);
