377e6361b5d5fe45b4e91151bb6dd012cec7a5e0
George Soosai <george_a_soosai@homedepot.com>
2019-06-21 16:35:52 -0400

    making friends to be dynamic

--- src/app/user/user-home/user-home.component.ts
+++ src/app/user/user-home/user-home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import {fas, faPlusCircle} from '@fortawesome/free-solid-svg-icons';
+import {fas, faPlusCircle, faMinusCircle} from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { UserProfileService } from '../service/user-profile.service';
 import { UserProfile } from '../model/user-profile';
@@ -15,9 +15,9 @@ import { NgForm } from '@angular/forms';
 })
 export class UserHomeComponent implements OnInit, AfterViewInit {
   userProfileList: Observable<UserProfile[]>;
-
+  friendsList: number[] = [];
   constructor(private modalService: NgbModal, private userProfileServoice: UserProfileService) {
-    library.add(fas, faCoffee, faPlusCircle);
+    library.add(fas, faCoffee, faPlusCircle, faMinusCircle);
    }
 
   ngOnInit() {
@@ -25,15 +25,34 @@ export class UserHomeComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    
+
+  }
+
+  addNewFriends() {
+    this.friendsList.push(this.friendsList.length + 1);
+  }
+  removeFriends(index: number) {
+    this.friendsList.splice(index, 1);
+  }
+
+  getFriends(formValue): string[] {
+    const keysList = Object.keys(formValue);
+    const results: string[] = [];
+    for (const key of keysList) {
+      if (key.search('friends') > -1) {
+        results.push(formValue[key]);
+      }
+    }
+    return results;
   }
 
   createUser(content) {
+    this.friendsList = [1];
     this.modalService.open(content, { size: 'lg', backdropClass: 'light-blue-backdrop' })
     .result.then(
       (userInput: NgForm) => {
         this.userProfileServoice.addNewUser(new UserProfile(userInput.form.value.userName,
-                                            [userInput.form.value.friends],
+                                            this.getFriends(userInput.form.value),
                                             userInput.form.value.age,
                                             userInput.form.value.weight));
       },
